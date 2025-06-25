ef5964364b59d081fce03a3ed3da7e9ec2a0aa64
i <i@users.noreply.github.com>
2025-06-25 17:01:39 +0800

    bump version to v25.0625.1701

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,11 +1,9 @@
 import { ThemeProvider } from './theme';
-import { GoogleAnalytics } from '@next/third-parties/google';
 import { QueryProvider } from './query';
 export default function Page({ children }: React.PropsWithChildren) {
   return (
     <ThemeProvider>
       <QueryProvider>{children}</QueryProvider>
-      <GoogleAnalytics gaId="G-Q3LLPR6HBQ" />
     </ThemeProvider>
   );
 }
